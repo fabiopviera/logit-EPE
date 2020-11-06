ac08026ad54a63076f661a08b3f0f619db85c0a3
Fábio Prataviera <fabiopviera@hotmail.com>
2020-11-06 19:43:54 -0300

    update

--- R/Application_1.R
+++ R/Application_1.R
@@ -8,44 +8,44 @@ data1 <- read.table("data_applications_1_2.txt",header=T,dec=",")
 str(data1)
 
 
-y <- data1$sw/100
-
+y <- data1$rh/100
+sd(log(y/(1-y)))
 #Descriptive
 cbind(round(c(mean=mean(y),median=median(y),sd=sd(y),min=min(y),max=max(y),skew=skewness(y),kurtosi=kurtosis(y)),3))
 
 
 ##logit-PE
-fit.logit.PE <- gamlss(y ~1,family = EPE01(mu.link = "identity",sigma.link = "identity",nu.link = "identity"
-),method=CG(1000),c.crit=0.01, tau.start = 1,tau.fix = T,mu.start =0.08)
+fit.logit.PE <- gamlss(y ~1,family = EPE01(mu.link = "identity",sigma.link = "identity",nu.link = "identity"),method=CG(1000),c.crit=0.01, 
+                       tau.start = 1,tau.fix = T,mu.start =0.5,sigma.start = 0.7,nu.start = 4)
 summary(fit.logit.PE)
 
 ##logit-EL
 fit.logit.EL <- gamlss(y~1,family = EPE01(mu.link = "identity",sigma.link = "identity",tau.link = "identity"),
-method=CG(1000),c.crit=0.01,nu.start = 1,nu.fix = T,mu.start =0.08)
+                       method=CG(1000),c.crit=0.01,nu.start = 1,nu.fix = T,mu.start =0.544)
 summary(fit.logit.EL)
 
 
 ##logit-ENO
 fit.logit.ENO <- gamlss(y~1,family = EPE01(mu.link = "identity",sigma.link = "identity",tau.link = "identity"),
-method=CG(1000),c.crit=0.01,nu.fix = T,mu.start =0.08)
+                        method=CG(1000),c.crit=0.01,nu.fix = T,mu.start =0.5,sigma.start = 0.7)
 summary(fit.logit.ENO)
 
 
 ##logit-L
-fit.logit.L <- gamlss(y~1,family = EPE01(mu.link = "identity",sigma.link = "identity",nu.link = "identity",tau.link = "identity"),
-method=CG(1000),c.crit=0.01,nu.start = 1,nu.fix = T,tau.start = 1,tau.fix = T,mu.start =0.08)
+fit.logit.L <- gamlss(y~1,family = EPE01(mu.link = "identity",sigma.link = "identity"),
+                      method=CG(1000),c.crit=0.01,nu.start = 1,nu.fix = T,tau.start = 1,tau.fix = T,mu.start =0.5,sigma.start = 0.7)
 summary(fit.logit.L)
 
 
 ##logit-ENO
-fit.logit.NO <- gamlss(y~1,family = EPE01(mu.link = "identity",sigma.link = "identity",nu.link = "identity"),
-method=CG(1000),c.crit=0.01,nu.fix = T,tau.start = 1,tau.fix = T,mu.start =0.08)
+fit.logit.NO <- gamlss(y~1,family = EPE01(mu.link = "identity",sigma.link = "identity"),
+                       method=CG(1000),c.crit=0.01,nu.fix = T,tau.start = 1,tau.fix = T,mu.start =0.54,sigma.start = 0.712)
 summary(fit.logit.NO)
 
 
 ##logit - EPE
 fit.logit.EPE <- gamlss(y ~1,family = EPE01(mu.link = "identity",sigma.link = "identity",nu.link = "identity",tau.link = "identity"),
-method=CG(1000),c.crit=0.01,mu.start = 0.08)
+                        method=CG(1000),c.crit=0.01,mu.start =0.5,sigma.start = 0.7)
 summary(fit.logit.EPE)
 
 
@@ -96,22 +96,23 @@ summary(fit.simplex)
 
 
 
+
 x11()
-hist(y,freq=F,xlab = "Soil water content in the 0-1 m soil layer",ylab = "f(y)",cex.lab=1.35,cex.axis=1.35,main="");box()
+hist(y,freq=F,xlab = "Relative humidity",ylab = "f(y)",cex.lab=1.35,cex.axis=1.35,main="");box()
 curve(dEPE01(x, mu=fit.logit.EPE$mu.coefficients,sigma=fit.logit.EPE$sigma.coefficients,nu=fit.logit.EPE$nu.coefficients,tau=fit.logit.EPE$tau.coefficients), add=T,lwd=4,col="gray15")
 curve(dBE(x, mu=fit.BE$mu.coefficients,sigma=fit.BE$sigma.coefficients), add=T,lwd=4,col="gray30",lty=2)
 curve(dSIMPLEX(x, mu=fit.simplex$mu.coefficients,sigma=fit.simplex$sigma.coefficients), add=T,lwd=4,col="gray45",lty=4)
 curve(dGB1(x, mu=fit.GBE$mu.coefficients,sigma=fit.GBE$sigma.coefficients,nu=fit.GBE$nu.coefficients,tau=fit.GBE$tau.coefficients), add=T,lwd=3,col="gray60")
-legend("topright",c("logit-EPE","BE","Simplex","GBE"),lwd=c(3,3,3,3),lty=c(1,2,4,1),col=c("gray15","gray30","gray45","gray60"),bty='n',cex=1.2)
-
+legend("topleft",c("logit-EPE","BE","Simplex","GBE"),lwd=c(3,3,3,3),lty=c(1,2,4,1),col=c("gray15","gray30","gray45","gray60"),bty='n',cex=1.2)
 
 x11()
-plot(ecdf(y),col="gray40",xlab = "Soil water content in the 0-1 m soil layer",ylab = "F(y)",cex.lab=1.35,cex.axis=1.35,main="")
+plot(ecdf(y),col="gray40",xlab = "Relative humidity",ylab = "F(y)",cex.lab=1.35,cex.axis=1.35,main="")
 curve(pEPE01(x, mu=fit.logit.EPE$mu.coefficients,sigma=fit.logit.EPE$sigma.coefficients,nu=fit.logit.EPE$nu.coefficients,tau=fit.logit.EPE$tau.coefficients), add=T,lwd=4,col="gray15")
 curve(pBE(x, mu=fit.BE$mu.coefficients,sigma=fit.BE$sigma.coefficients), add=T,lwd=4,col="gray30",lty=2)
 curve(pSIMPLEX(x, mu=fit.simplex$mu.coefficients,sigma=fit.simplex$sigma.coefficients), add=T,lwd=4,col="gray45",lty=4)
 curve(pGB1(x, mu=fit.GBE$mu.coefficients,sigma=fit.GBE$sigma.coefficients,nu=fit.GBE$nu.coefficients,tau=fit.GBE$tau.coefficients), add=T,lwd=3,col="gray60")
 legend("topleft",c("Empirical distribution","logit-EPE","BE","Simplex","GBE"),lwd=c(3,3,3,3,3),lty=c(1,1,2,4,1),col=c("gray40","gray15","gray30","gray45","gray60"),bty='n',cex=1.2)
+
 
 
 Res.q1 <- fit.logit.EPE$residuals
